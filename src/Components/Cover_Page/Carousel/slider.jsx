65585bb84257733fef7b6d3a6d8f6f3721ed923b
utkarsh619verma <utkarsh619verma@gmail.com>
2025-07-23 02:16:16 +0530

    made more responsive changes for tablet and mobile screen

--- src/Components/Cover_Page/Carousel/slider.jsx
+++ src/Components/Cover_Page/Carousel/slider.jsx
@@ -4,14 +4,14 @@ import SliderContent from "./sliderContent";
 import style from "./carousel.module.css";
 
 export default function Slider() {
-  const [activeIndex, setActiveIndex] = useState(2);
+  const [activeIndex, setActiveIndex] = useState(0);
   const arrayLength = sliderImage.length;
-  // useEffect(() => {
-  //   let interval = setInterval(() => {
-  //     setActiveIndex((activeIndex) => (activeIndex + 1) % arrayLength);
-  //   }, 6000);
-  //   return () => clearInterval(interval);
-  // }, []);
+  useEffect(() => {
+    let interval = setInterval(() => {
+      setActiveIndex((activeIndex) => (activeIndex + 1) % arrayLength);
+    }, 6000);
+    return () => clearInterval(interval);
+  }, []);
   return (
     <div className={style.sliderContainer}>
       <SliderContent activeIndex={activeIndex} sliderImage={sliderImage} />
